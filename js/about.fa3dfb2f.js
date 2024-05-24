b2697dab9c44a215f335434646ceccbbcf97bdbc
hardhus <ahmetcemiltoprakk@gmail.com>
2024-05-25 02:16:26 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   css/app.ddbde395.css
    	new file:   favicon.ico
    	new file:   img/icons/android-chrome-192x192.png
    	new file:   img/icons/android-chrome-512x512.png
    	new file:   img/icons/android-chrome-maskable-192x192.png
    	new file:   img/icons/android-chrome-maskable-512x512.png
    	new file:   img/icons/apple-touch-icon-120x120.png
    	new file:   img/icons/apple-touch-icon-152x152.png
    	new file:   img/icons/apple-touch-icon-180x180.png
    	new file:   img/icons/apple-touch-icon-60x60.png
    	new file:   img/icons/apple-touch-icon-76x76.png
    	new file:   img/icons/apple-touch-icon.png
    	new file:   img/icons/favicon-16x16.png
    	new file:   img/icons/favicon-32x32.png
    	new file:   img/icons/msapplication-icon-144x144.png
    	new file:   img/icons/mstile-150x150.png
    	new file:   img/icons/safari-pinned-tab.svg
    	new file:   index.html
    	new file:   js/about.fa3dfb2f.js
    	new file:   js/about.fa3dfb2f.js.map
    	new file:   js/app.022cb819.js
    	new file:   js/app.022cb819.js.map
    	new file:   js/chunk-vendors.873d1856.js
    	new file:   js/chunk-vendors.873d1856.js.map
    	new file:   manifest.json
    	new file:   robots.txt
    	new file:   service-worker.js
    	new file:   service-worker.js.map
    	new file:   workbox-6567b62a.js
    	new file:   workbox-6567b62a.js.map

---
+++ js/about.fa3dfb2f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[594],{9147:function(n,t,u){u.r(t),u.d(t,{default:function(){return l}});var o=u(6768);const r={class:"about"},a=(0,o.Lk)("h1",null,"This is an about page",-1),e=[a];function s(n,t){return(0,o.uX)(),(0,o.CE)("div",r,e)}var c=u(1241);const i={},f=(0,c.A)(i,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.fa3dfb2f.js.map
